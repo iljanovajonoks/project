3263424dea5731702cdacf44228aff0c751fb8b9
Ilja Novajonoks <144263108+iljanovajonoks@users.noreply.github.com>
2024-01-10 14:46:49 +0000

    10.01.2024

--- main.py
+++ main.py
@@ -15,27 +15,41 @@ driver = webdriver.Chrome(service=service, options=option)
 url = "https://coinmarketcap.com/"
 driver.get(url)
 
-file = pandas.read_excel("portfolio.xlsx")
-portfolio = file.values.tolist()
+# Pieņemu visus sīkfailus.
+find = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "onetrust-accept-btn-handler")))
+find.click()
 
-current_pice = []
+# Excel atveršana ar portfeļa datiem
+file_path = "portfolio.xlsx"
+df = pandas.read_excel(file_path)
+portfolio = df.values.tolist()
+
+current_price = []
+
 for row in portfolio:
 
     # Mēģinu meklēt 2 klases, jo ir 2 veidu meklēšanas pogas atkarībā no pārlūkprogrammas loga izmēra.
     try:
-        find = WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-aef7b723-0.fKbUaI")))
+        find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-aef7b723-0.fKbUaI")))
 
     except:
-        find = WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-e20acb0c-1.fWcxPm")))
+        find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-e20acb0c-1.fWcxPm")))
 
     find.click()
 
-    find = WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-8829bc3d-3.ikgYUd")))
+    # Meklēšanas loga ievadu kriptovalūtas nosaukumus.
+    find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-8829bc3d-3.ikgYUd")))
     find.send_keys(row[1])
     time.sleep(1)
     find.send_keys(Keys.RETURN)
-    find = WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-f70bb44c-0.jxpCgO.base-text")))
-    temp = (find.text).replace("$", "").replace(",", "")
-    current_pice.append(float(temp))
-    print(current_pice)
-input()
+
+    # Atradu kriptovalūtas cenu un noņemu nevajadzīgus simbolus.
+    find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-f70bb44c-0.jxpCgO.base-text")))
+    temp = find.text.replace("$", "").replace(",", "")
+    current_price.append(float(temp))
+    print(current_price)
+
+df['Current Price'] = current_price
+df.to_excel(file_path, index=False)
+
+driver.quit()
