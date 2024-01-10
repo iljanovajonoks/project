7c158e4ecfefcd427a261f9c0c97fb7b0a0bd3be
Ilja Novajonoks <144263108+iljanovajonoks@users.noreply.github.com>
2024-01-10 15:38:20 +0000

    10.01.2024

--- main.py
+++ main.py
@@ -20,9 +20,18 @@ find = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 
 find.click()
 
 # Excel atveršana ar portfeļa datiem
-file_path = "portfolio.xlsx"
+file_path = "portfolio2.xlsx"
 df = pandas.read_excel(file_path)
 portfolio = df.values.tolist()
+
+# Sākotnējā ieguldījuma lieluma aprēķināšana un tās ierakstīšana Excel failā.
+invested_amount = []
+
+for row in portfolio:
+    invested_amount.append(row[2] * row[3])
+
+df['Invested amount, $'] = invested_amount
+df.to_excel(file_path, index=False)
 
 current_price = []
 
@@ -30,26 +39,49 @@ for row in portfolio:
 
     # Mēģinu meklēt 2 klases, jo ir 2 veidu meklēšanas pogas atkarībā no pārlūkprogrammas loga izmēra.
     try:
-        find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-aef7b723-0.fKbUaI")))
+        find = WebDriverWait(driver, 1).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "sc-aef7b723-0.fKbUaI")))
 
     except:
-        find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-e20acb0c-1.fWcxPm")))
+        find = WebDriverWait(driver, 1).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "sc-e20acb0c-1.fWcxPm")))
 
     find.click()
 
     # Meklēšanas loga ievadu kriptovalūtas nosaukumus.
-    find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-8829bc3d-3.ikgYUd")))
+    find = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-8829bc3d-3.ikgYUd")))
     find.send_keys(row[1])
     time.sleep(1)
     find.send_keys(Keys.RETURN)
 
-    # Atradu kriptovalūtas cenu un noņemu nevajadzīgus simbolus.
-    find = WebDriverWait(driver, 0.5).until(EC.visibility_of_element_located((By.CLASS_NAME, "sc-f70bb44c-0.jxpCgO.base-text")))
+    # Atradu kriptovalūtas cenu, noņemu nevajadzīgus simbolus un ierakstu cenu excel failā.
+    find = WebDriverWait(driver, 1).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "sc-f70bb44c-0.jxpCgO.base-text")))
     temp = find.text.replace("$", "").replace(",", "")
     current_price.append(float(temp))
     print(current_price)
 
-df['Current Price'] = current_price
+df['Current Price, $'] = current_price
+df.to_excel(file_path, index=False)
+
+# Peļņas vai zaudējumu aprēķins no pirkuma cenas izmaiņām, procentos.
+profit_loss = []
+for row in portfolio:
+    temp = ((row[5] - row[3]) / row[3]) * 100
+    profit_loss.append(round(temp, 2))
+print(profit_loss)
+
+df['Profit / Loss, %'] = profit_loss
+df.to_excel(file_path, index=False)
+
+# Pašreizējais investīciju apjoma aprēķināšana un ierakstīšana excel failā.
+current_invest_amount = []
+for row in portfolio:
+    temp = (row[2] * row[5])
+    current_invest_amount.append(round(temp, 2))
+print(current_invest_amount)
+
+df['Current Invested Amount, $'] = current_invest_amount
 df.to_excel(file_path, index=False)
 
 driver.quit()
